d7f6961a0b901ce1231b9e5c0cbe47e5324152e0
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2023-08-14 22:27:46 +0900

    varセット

--- variables.tf
+++ variables.tf
@@ -1,5 +1,9 @@
 variable "TFC_AWS_RUN_ROLE_ARN" {
   description = "The ARN of the AWS Role for Terraform Cloud"
   type        = string
-  // default     = "some_default_value" // もしデフォルト値が必要な場合に追加します
+}
+
+variable "TFC_AWS_PROVIDER_AUTH" {
+  description = "TFC_AWS_PROVIDER_AUTH"
+  type        = string
 }
