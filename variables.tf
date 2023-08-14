b1f22456a6c4833f951528c621140999ad48398b
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2023-08-14 22:15:51 +0900

    wip

--- variables.tf
+++ variables.tf
@@ -1,6 +1,3 @@
-
-main.tfファイルの内容から、TFC_AWS_RUN_ROLE_ARNという変数は現在宣言されていないようです。もしこの変数を使用する場合、まずその変数を宣言する必要があります。
-
 variable "TFC_AWS_RUN_ROLE_ARN" {
   description = "The ARN of the AWS Role for Terraform Cloud"
   type        = string
