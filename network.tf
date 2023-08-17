66bba2ece4f53e4d0e8375d4297fd97e17a87444
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2023-08-17 09:38:35 +0900

    再チャレンジ

--- network.tf
+++ network.tf
@@ -9,6 +9,8 @@ resource "aws_vpc" "test_vpc" {
 
 resource "aws_subnet" "elb-public1" {
   vpc_id            = aws_vpc.test_vpc.id
+  cidr_block        = "172.31.102.0/24"
+  availability_zone = "ap-northeast-1a"
 
   tags = {
     Name = "elb-public1" 
