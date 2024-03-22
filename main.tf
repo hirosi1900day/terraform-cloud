6c71cbdd792cfc93f5c596c0dc34db7df4241bc1
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-03-22 22:15:43 +0900

    backendに修正

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
 terraform {
-  cloud {
-    organization = "test-horosi1900day"
-
-    workspaces {
-      name = "terraform-cloud"
-    }
-  }
-
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "test-horosi1900day"
