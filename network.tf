08183cd27d65d475ef8a6b0d0e253bdbf4dc1d61
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-04-23 15:21:39 +0900

    一旦検証ネットワークをコメントアウトする

--- network.tf
+++ network.tf
@@ -1,51 +1,51 @@
-data "terraform_remote_state" "vpc" {
-  backend = "remote"
-  config = {
-    organization = "test-horosi1900day"
-    workspaces = {
-      name = "terraform-cloud-state-test"
-    }
-  }
-}
+# data "terraform_remote_state" "vpc" {
+#   backend = "remote"
+#   config = {
+#     organization = "test-horosi1900day"
+#     workspaces = {
+#       name = "terraform-cloud-state-test"
+#     }
+#   }
+# }
 
-resource "aws_vpc" "test_vpc2" {
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+# resource "aws_vpc" "test_vpc2" {
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
 
-  tags = {
-    Name = "menta-vpc2"
-  }
-}
+#   tags = {
+#     Name = "menta-vpc2"
+#   }
+# }
 
-resource "aws_subnet" "elb-public1" {
-  vpc_id            = aws_vpc.test_vpc2.id
-  cidr_block        = "172.31.102.0/24"
-  availability_zone = "ap-northeast-1a"
+# resource "aws_subnet" "elb-public1" {
+#   vpc_id            = aws_vpc.test_vpc2.id
+#   cidr_block        = "172.31.102.0/24"
+#   availability_zone = "ap-northeast-1a"
 
-  tags = {
-    Name = "elb-public1"
-  }
-}
+#   tags = {
+#     Name = "elb-public1"
+#   }
+# }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_http"
-  description = "state shared security group"
-  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+# resource "aws_security_group" "allow_tls" {
+#   name        = "allow_http"
+#   description = "state shared security group"
+#   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
-  tags = {
-    Name = "allow_tls"
-  }
-}
+#   tags = {
+#     Name = "allow_tls"
+#   }
+# }
 
-moved {
-  from = aws_vpc.test_vpc
-  to   = aws_vpc.test_vpc2
-}
+# moved {
+#   from = aws_vpc.test_vpc
+#   to   = aws_vpc.test_vpc2
+# }
 
-output "test" {
-  value       = data.terraform_remote_state.vpc.outputs.vpc_id
-  description = "ステート共有確認"
-}
+# output "test" {
+#   value       = data.terraform_remote_state.vpc.outputs.vpc_id
+#   description = "ステート共有確認"
+# }
 
 # module "vpc" {
 #   source  = "app.terraform.io/test-horosi1900day/vpc/aws"
