d6b8242ca32bc4c21206fb94e26552535a8709e3
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-05-01 14:44:45 +0900

    DBユーザー設定追加

--- aurora2.tf
+++ aurora2.tf
@@ -162,19 +162,22 @@ resource "aws_rds_cluster_parameter_group" "aurora_mysql_for_test" {
 }
 
 resource "aws_rds_cluster" "rds_cluster_for_test" {
-  engine                          = data.aws_rds_engine_version.aurora_cluster_for_test.engine
-  engine_version                  = data.aws_rds_engine_version.aurora_cluster_for_test.version
-  cluster_identifier              = "test-cluster-for-test"
-  storage_encrypted               = true
+  engine             = data.aws_rds_engine_version.aurora_cluster_for_test.engine
+  engine_version     = data.aws_rds_engine_version.aurora_cluster_for_test.version
+  cluster_identifier = "test-cluster-for-test"
+  storage_encrypted  = true
   # kms_key_id                      = aws_kms_key.rds.arn
-  backup_retention_period         = 1
-  preferred_backup_window         = "17:00-17:30"
-  port                            = 3306
-  skip_final_snapshot             = true
-  deletion_protection             = false
-  allow_major_version_upgrade     = true
-  apply_immediately               = true
+  backup_retention_period     = 1
+  preferred_backup_window     = "17:00-17:30"
+  port                        = 3306
+  skip_final_snapshot         = true
+  deletion_protection         = false
+  allow_major_version_upgrade = true
+  apply_immediately           = true
   # vpc_security_group_ids          = [aws_security_group.rds.id]
+  database_name                   = "mydb"
+  master_username                 = "foo"
+  master_password                 = "bar"
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.aurora_mysql_for_test.name
   enabled_cloudwatch_logs_exports = [
     "error",
