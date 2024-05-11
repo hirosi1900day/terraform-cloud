d2bbae87d0d87912ca07f837730d008664ea738a
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-05-11 16:58:21 +0900

    一回元に戻す

--- s3.tf
+++ s3.tf
@@ -1,3 +1,21 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "cdacdscsacsacdcsdmy-tf-example-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
 # module "s3_bucket" {
 #   source = "terraform-aws-modules/s3-bucket/aws"
 
@@ -8,14 +26,8 @@
 #   object_ownership         = "BucketOwnerPreferred"
 # }
 
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "cdcdcddsacdscsacsacdcsdmy-tf-example-bucket"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "BucketOwnerPreferred"
-}
-
 # TODO: stateの移行が完了したら削除する
+moved {
+  from = aws_s3_bucket.example
+  to   = module.s3_bucket
+}
