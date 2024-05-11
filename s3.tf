73aa054d73d7986ef79cbe805659644c16f6f219
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-05-11 16:33:36 +0900

    wip

--- s3.tf
+++ s3.tf
@@ -15,3 +15,19 @@ resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "cdacdscsacsacdcsdmy-tf-example-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+}
+
+# TODO: stateの移行が完了したら削除する
+moved {
+  from = aws_s3_bucket.example
+  to   = module.s3_bucket
+}
