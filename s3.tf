cb2c859335d68df872677b54ee2083175c4bd72a
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-05-11 17:02:47 +0900

    どうかな？？

--- s3.tf
+++ s3.tf
@@ -1,33 +1,43 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "cdacdscsacsacdcsdmy-tf-example-bucket"
-}
-
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
-
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
-}
-
-# module "s3_bucket" {
-#   source = "terraform-aws-modules/s3-bucket/aws"
-
+# resource "aws_s3_bucket" "example" {
 #   bucket = "cdacdscsacsacdcsdmy-tf-example-bucket"
-#   acl    = "private"
-
-#   control_object_ownership = true
-#   object_ownership         = "BucketOwnerPreferred"
 # }
+
+# resource "aws_s3_bucket_ownership_controls" "example" {
+#   bucket = aws_s3_bucket.example.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
+
+# resource "aws_s3_bucket_acl" "example" {
+#   depends_on = [aws_s3_bucket_ownership_controls.example]
+
+#   bucket = aws_s3_bucket.example.id
+#   acl    = "private"
+# }
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "cdacdscsacsacdcsdmy-tf-example-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+}
 
 # TODO: stateの移行が完了したら削除する
-# moved {
-#   from = aws_s3_bucket.example
-#   to   = module.s3_bucket
-# }
+moved {
+  from = aws_s3_bucket.example
+  to   = module.s3_bucket.aws_s3_bucket.this[0]
+}
+
+moved {
+  from = aws_s3_bucket_acl.example
+  to   = module.s3_bucket.aws_s3_bucket_acl.this[0]
+}
+
+moved {
+  from = aws_s3_bucket_ownership_controls.example
+  to   = module.s3_bucket.aws_s3_bucket_ownership_controls.this[0]
+}
