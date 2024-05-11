7361a7ce834f07290f8de3ddcd974e9bd9454293
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-05-11 16:54:55 +0900

    wip

--- s3.tf
+++ s3.tf
@@ -1,13 +1,21 @@
+# module "s3_bucket" {
+#   source = "terraform-aws-modules/s3-bucket/aws"
+
+#   bucket = "cdacdscsacsacdcsdmy-tf-example-bucket"
+#   acl    = "private"
+
+#   control_object_ownership = true
+#   object_ownership         = "BucketOwnerPreferred"
+# }
+
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "cdacdscsacsacdcsdmy-tf-example-bucket"
+  bucket = "cdcdcddsacdscsacsacdcsdmy-tf-example-bucket"
   acl    = "private"
 
   control_object_ownership = true
   object_ownership         = "BucketOwnerPreferred"
 }
-
-
 
 # TODO: stateの移行が完了したら削除する
