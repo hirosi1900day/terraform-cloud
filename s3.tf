2836f5eb0520a806d51ca13140e0d2a5bb815735
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-05-12 20:36:38 +0900

    一旦消すよ~~

--- s3.tf
+++ s3.tf
@@ -16,13 +16,13 @@
 #   acl    = "private"
 # }
 
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+# module "s3_bucket" {
+#   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "cdacdscsacsacdcsdmy-tf-example-bucket"
-  acl    = "private"
-  attach_public_policy = false
+#   bucket = "cdacdscsacsacdcsdmy-tf-example-bucket"
+#   acl    = "private"
+#   attach_public_policy = false
 
-  control_object_ownership = true
-  object_ownership         = "BucketOwnerPreferred"
-}
+#   control_object_ownership = true
+#   object_ownership         = "BucketOwnerPreferred"
+# }
