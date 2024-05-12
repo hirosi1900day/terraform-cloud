dd833ec57c70f3f91935f79590901711cded2f47
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-05-12 20:43:23 +0900

    もう一回登録してみるよ〜〜

--- s3.tf
+++ s3.tf
@@ -1,20 +1,20 @@
-# resource "aws_s3_bucket" "example" {
-#   bucket = "cdacdscsacsacdcsdmy-tf-example-bucket"
-# }
+resource "aws_s3_bucket" "example" {
+  bucket = "cdacdscsacsacdcsdmy-tf-example-bucket"
+}
 
-# resource "aws_s3_bucket_ownership_controls" "example" {
-#   bucket = aws_s3_bucket.example.id
-#   rule {
-#     object_ownership = "BucketOwnerPreferred"
-#   }
-# }
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
-# resource "aws_s3_bucket_acl" "example" {
-#   depends_on = [aws_s3_bucket_ownership_controls.example]
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
 
-#   bucket = aws_s3_bucket.example.id
-#   acl    = "private"
-# }
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
 
 # module "s3_bucket" {
 #   source = "terraform-aws-modules/s3-bucket/aws"
